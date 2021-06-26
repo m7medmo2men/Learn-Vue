0e5e8547d061694ff7067e562feefdfc03eae5cd
Mohamed Momen <m7medmo2men@gmail.com>
2021-06-26 11:33:50 +0200

    initial files

---
+++ bot-project2/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: false,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "@vue/airbnb"],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+    "linebreak-style": 0,
+    "quotes": [2, "double", { "avoidEscape": true }],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
+};
